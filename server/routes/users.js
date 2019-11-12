36d78eab1c27bf0b58c429bc5284f82b7b3a4232
Satyo Wicaksana <satyowicaksana@gmail.com>
2019-11-12 17:43:59 +0700

    clean repo

--- server/routes/users.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const UserController = require('../controllers/user');
-
-router.post('/signup', UserController.signup);
-router.post('/signin', UserController.signin);
-
-module.exports = router;
