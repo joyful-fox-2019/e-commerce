69a61466892d98831ea924dc14a082b11949d084
Edison <edirates@gmail.com>
2019-11-15 22:01:42 +0700

    Initial commit

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,29 +1,12 @@
 <template>
   <div id="app">
-    <!-- <div id="nav">
+    <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-    </div> -->
-    <NavBar></NavBar>
+    </div>
     <router-view/>
   </div>
 </template>
-
-<script>
-import NavBar from './components/NavBar'
-
-export default {
-  name: 'App',
-  data: function () {
-    return {
-      isLogin: false
-    }
-  },
-  components: {
-    NavBar
-  }
-}
-</script>
 
 <style>
 #app {
